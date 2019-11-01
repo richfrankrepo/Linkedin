a8b576174a838614baf56808ba7c69da12a3fdc9
richfrankrepo <47148662+richfrankrepo@users.noreply.github.com>
2019-11-01 13:58:33 +0000

    Add files via upload

---
+++ AzureStuff/ExportTemplates/Export-All-RG.ps1
@@ -0,0 +1,25 @@
+connect-azaccount
+
+$colSubscriptions = Get-AZsubscription
+
+foreach($subscription in $colSubscriptions)
+{
+    
+    $path = ".\" + ($subscription.name).replace("/", "-")
+    $path = ($path).replace("\\", "-")
+    
+
+    if (-not(Test-Path $path) )
+    {
+        New-Item -Path $path -ItemType Directory
+    }
+    
+    select-azsubscription $subscription.name
+    $colRG = get-azresourcegroup
+
+    foreach ($rg in $colRG)
+    {
+       #export-azresourcegroup -ResourceGroupName $rg.resourcegroupname -Path $path -Force -SkipAllParameterization
+    }
+}
+
